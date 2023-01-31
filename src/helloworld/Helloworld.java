02a53bb445fe343c6224556f94a06ad03fc912b4
jaska <jaska@10.48.26.239>
2023-01-31 14:21:38 -0500

    day1

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,17 @@
+
+package helloworld;
+
+/**
+ *
+ * @author jaska
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 
+    }
+
+}
